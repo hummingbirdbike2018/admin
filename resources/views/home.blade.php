b0b6d883834549ca12ceb6dcfbdec1f456915a84
鈴木将彦 <ternbicycles2013@gmail.com>
2019-07-25 01:07:31 +0900

    管理者ログインのロジック簡略化 管理メニュー作成

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,45 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="row mb-4">
+		<div class="col-sm-6">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">会員管理</h5>
+					<p class="card-text">会員情報閲覧・ステータスの変更</p>
+					<a href="{{ route('member.list') }}" class="btn btn-primary">会員管理画面へ</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">起案者管理</h5>
+					<p class="card-text">起案者情報閲覧・ステータスの変更</p>
+					<a href="{{ route('drafter.list') }}" class="btn btn-primary">起案者管理画面へ</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-6">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">プロジェクト管理</h5>
+					<p class="card-text">既存プロジェクトの変更・取り消し</p>
+					<a href="{{ route('project.list') }}" class="btn btn-primary">プロジェクト管理画面へ</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">プロジェクト作成</h5>
+					<p class="card-text">新規プロジェクト作成・投稿</p>
+					<a href="{{ route('project.create') }}" class="btn btn-primary">プロジェクト作成画面へ</a>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,31 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function() {
+	//ログイン
+	Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
+	Route::post('login', 'Auth\LoginController@login')->name('login');
+	//登録
+	Route::get('register', 'Auth\RegisterController@showRegisterForm')->name('register');
+	Route::post('register', 'Auth\RegisterController@register')->name('register');
+	//パスワードリセット
+	Route::get('password/rest', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+	//ログアウト
+	Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+	Route::get('home', 'HomeController@index')->name('home');
+	//会員管理
+	Route::get('member/management', 'Auth\MemberController@showMemberList')->name('member.list');
+	Route::post('member/management', 'Auth\MemberController@edit')->name('member.edit');
+	//起案者管理
+	Route::get('drafter/management', 'Auth\DrafterController@showDrafterList')->name('drafter.list');
+	Route::post('drafter/management', 'Auth\DrafterController@edit')->name('drafter.edit');
+	//プロジェクト管理
+	Route::get('project/management', 'Auth\ProjectController@showProjectList')->name('project.list');
+	Route::post('project/management', 'Auth\ProjectController@edit')->name('project.edit');
+	Route::post('project/management', 'Auth\ProjectController@delete')->name('project.delete');
+	//プロジェクト作成
+	Route::get('project/create', 'Auth\ProjectController@showRegisterForm')->name('project.create');
+	Route::post('project/create', 'Auth\ProjectController@create')->name('project.create');
 });
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
