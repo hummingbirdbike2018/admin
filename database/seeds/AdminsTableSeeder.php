7bfb0d4698006da5d71e60d26b1cf19197b5338f
鈴木将彦 <ternbicycles2013@gmail.com>
2019-07-31 02:29:14 +0900

    管理者テーブルマイグレーション、シーダ作成

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -1,16 +1,24 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class AdminsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+		/**
+		 * Run the database seeds.
+		 *
+		 * @return void
+		 */
+		public function run()
+		{
+			DB::table('admins')->insert([
+				'id' => 1,
+				'email' => 'admin@email.com',
+				'password' => bcrypt('test1234'),
+				'name' => '管理者アカウント',
+				'created_at' => Carbon::now(),
+				'updated_at' => Carbon::now(),
+			]);
+		}
 }
