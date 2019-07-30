c71033c84bd8109b89f136fbca2eb83ea1b29b70
鈴木将彦 <ternbicycles2013@gmail.com>
2019-07-31 02:29:14 +0900

    管理者テーブルマイグレーション、シーダ作成

--- database/migrations/2019_07_24_124502_create_admins_table.php
+++ database/migrations/2019_07_24_124502_create_admins_table.php
@@ -6,31 +6,33 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateAdminsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('admins', function (Blueprint $table) {
-          $table->increments('id');
-          $table->string('name');
-          $table->string('email')->unique();
-          $table->timestamp('email_verified_at')->nullable();
-          $table->string('password');
-          $table->rememberToken();
-          $table->timestamps();
-        });
-    }
+		/**
+		 * Run the migrations.
+		 *
+		 * @return void
+		 */
+		public function up()
+		{
+				Schema::create('admins', function (Blueprint $table) {
+					$table->increments('id');
+					$table->string('name');
+					$table->string('email')->unique();
+					$table->timestamp('email_verified_at')->nullable();
+					$table->string('password');
+					$table->integer('status')->default(1);
+					$table->string('dis_reason')->nullable();
+					$table->rememberToken();
+					$table->timestamps();
+				});
+		}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('admins');
-    }
+		/**
+		 * Reverse the migrations.
+		 *
+		 * @return void
+		 */
+		public function down()
+		{
+				Schema::dropIfExists('admins');
+		}
 }
